b9b3911d1293cc4228340b780d1f6573de61df31
Ruan <gruan@iu.edu>
2024-08-26 15:17:21 -0400

    add back dictionaryutils folder

---
+++ dictionaryutils/docker/dictionaryutils/bin/dump_schema.py
@@ -0,0 +1,18 @@
+"""
+This script dumps all schema files in currently installed gdcdictionary
+to one json schema to ./artifacts folder.
+
+"""
+import json
+import os
+
+from gdcdictionary import SCHEMA_DIR
+from dictionaryutils import dump_schemas_from_dir
+
+try:
+    os.mkdir("artifacts")
+except OSError:
+    pass
+
+with open(os.path.join("artifacts", "schema.json"), "w") as f:
+    json.dump(dump_schemas_from_dir(SCHEMA_DIR), f)
