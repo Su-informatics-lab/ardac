6f9cf4431c95cc2591f3076c594db83f02ae4ad2
Alan Walsh <alwalsh@iu.edu>
2024-08-29 15:47:16 -0400

    Output pytest results for validation check (GH action)

--- dictionaryutils/docker/run_tests.sh
+++ dictionaryutils/docker/run_tests.sh
@@ -6,13 +6,6 @@ export SQLALCHEMY_SILENCE_UBER_WARNING=1
 # Activate the virtual environment
 source /app/venvs/dict/bin/activate
 
-#which python
-#which poetry
-
-#ls /app/venvs/dict/lib/python3.10/site-packages/*dictionary*
-#ls /app/venvs/dict/lib/python3.10/site-packages/*gdc*
-#ls /app/venvs/dict/lib/python3.10/site-packages/*dictionaryutils*
-
 # Define the host directory to mount
 HOST_DIR="/mnt/host"
 
@@ -30,14 +23,12 @@ fi
 
 # Copy YAML files from the host directory to the container's schema directory
 SCHEMA_DIR=$(python -c "from gdcdictionary import SCHEMA_DIR; print(SCHEMA_DIR)")
-#echo $SCHEMA_DIR
-
-#ls $HOST_DIR/gdcdictionary/schemas/*.yaml
 
 # Copy YAML files from the host directory to the container's schema directory
 cp $HOST_DIR/gdcdictionary/schemas/*.yaml $SCHEMA_DIR
 
-pytest tests -s -v
+# Run tests and save output for validation check
+pytest tests -s -v --junitxml=$HOST_DIR/results.xml
 
 # change dir to produce dictionary json
 cd $HOST_DIR
